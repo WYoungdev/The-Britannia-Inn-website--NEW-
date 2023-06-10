81c47ce1f12d47858ff318480f6fbba1359049e0
WYoungDev <you22003709@btc.ac.uk>
2023-06-10 18:06:22 +0100

    Wiped the javascript file ready to be rewritten.

--- dump.js
+++ dump.js
@@ -1,10 +1,1 @@
 // Script for the Google maps API //
-<script>
-  function initMap() {
-    var mapOptions = {
-      center: { lat: 51.195139923437374, lng: -3.4657318449463763 },
-      zoom: 16,
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  }
-</script>
