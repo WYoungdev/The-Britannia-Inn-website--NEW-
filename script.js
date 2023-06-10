52f52c5d8acfd2ed6b567351f42332b4cb6688f8
WYoungDev <you22003709@btc.ac.uk>
2023-06-10 18:30:22 +0100

    Redesigned the code for the google interactive map. Map still not displaying on the screen however.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Script for the Google maps API //
+
+// Initialising the map //
+var map = new google.maps.Map(document.getElementById("map"), {
+  center: new google.maps.LatLng(latitude, longitude),
+  zoom: 15
+});
+
+// Adding The Britannia Inn as a marker on the map //
+var marker = new google.maps.Marker({
+  position: new google.maps.LatLng(51.195160093748925, -3.465345606871718),
+  map: map
+});
