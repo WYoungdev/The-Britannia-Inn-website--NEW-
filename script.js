3c6792d6a9483a7184a806756fa927fcc092d02e
WYoungDev <you22003709@btc.ac.uk>
2023-07-26 00:02:09 +0100

    Committed again to fully commit all of the previous changes.

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+//This is the function to enable the dark mode button to function.
 function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-  }  
+  // Get the <body> element
+  const body = document.body;
+
+  // Toggle the 'dark-mode' class on the body
+  body.classList.toggle('dark-mode');
+}
+
+
+// This is the function for the navigation bar toggle button. The 'navbarlinks' variable was set to the 'navbarLinks' class in HTML to enable functionality
+// to the correct element on the webpage. A simple if/else statement was used to hide/show the navigation bar depending on the current state of the navigation
+//bar. If it is shown, when the toggle button is clicked, the reverse will be true.
+function toggleNav() {
+  var navbarLinks = document.getElementById("navbarLinks");
+  if (navbarLinks.style.display === "flex") {
+    navbarLinks.style.display = "none";
+  } else {
+    navbarLinks.style.display = "flex";
+  }
+}
+
+// An event listener was used to handle clicking on navigation links, so that the website knows the currently active tab.
+var navLinks = document.querySelectorAll(".navbar-links li a"); //This selects all of the elements within the navigation bar list and stores them in the navLinks variable.
+for (var i = 0; i < navLinks.length; i++) { //This starts a for-loop that iterates through every element in the newly created navLinks variable. The for loop increments by 1 for each index until the length of the variable items is reached.
+  navLinks[i].addEventListener("click", function () { //This line listens for the click of the user. When the user clicks the toggle button, the for loop below begins.
+    for (var j = 0; j < navLinks.length; j++) {
+      navLinks[j].classList.remove("active"); //This removes the 'active' class from all navigation options.
+    }
+    this.classList.add("active"); //This readds the 'active' class to whichever navigation option the user clicked, applying the 'active' class styling to that option and showing the user which webpage they are currently on.
+  }); //Ends the listener.
+}
